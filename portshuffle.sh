d27e9fa7253f08713a8bbdb2b890632d8e9132bf
P-C Markovski <pcwriter@wearingmedia.com>
2020-07-11 11:38:33 +0200

    portshuffle.sh updated with if-blocks to check for number of parameters and the order of parameters. $1 should be lower than $2.

--- portshuffle.sh
+++ portshuffle.sh
@@ -7,37 +7,46 @@
 # Purpose: Generate new random port for the SSHD daemon, reconfigure daemon and restart it.
 
 # ToDo:
-#	- Check how uniform the random number distribution is for..
-#		- $RANDOM*2 with >15000. (15k-65k)
-#		- $RANDOM*2 (max 65k)
-#		- /dev/urandom
-#	- Graph using ChartJS.
+#       - Check how uniform the random number distribution is for..
+#               - $RANDOM*2 with >15000. (15k-65k)
+#               - $RANDOM*2 (max 65k)
+#               - /dev/urandom
+#       - Graph using ChartJS.
 
 
-		# Global variables.
+                # Global variables.
 MINPORT=15000
 
-		# Create a new random port, in the interval specified by two parameters.
+
+
+                # Create a new random port, in the interval specified by two parameters.
 portshuffle()
 {
-	seed=$(($RANDOM*2))
+        seed=$(($RANDOM*2))
 
-	while [ $seed -lt $1 -o $seed -gt $2 ]; do
-		seed=$(($RANDOM*2))
-	done
+        while [ $seed -lt $1 -o $seed -gt $2 ]; do
+                seed=$(($RANDOM*2))
+        done
 
-	echo $seed
+        echo $seed
 }
 
-		# Change the port in the specified daemon.
+                # Change the port in the specified daemon.
 portchange()
 {
-		# Apply sed pattern and change the existing port to the random one.
-	sed -i s/.*Port.*/"Port $1"/g /etc/ssh/sshd_config
+                # Apply sed pattern and change the existing port to the random one.
+        sed -i s/.*Port.*/"Port $1"/g /etc/ssh/sshd_config
 }
 
-		# Run script.
-newport=$(portshuffle $1 $2)		# Generate a new random port.
-printf "Final: %s\n" $newport
-#portchange $newport		# Change the ssh port to newport in the ssh configuration file.
-#/etc/init.d/ssh restart		# Restart the daemon identified by a parameter.
+if [ $# -ne 2 ]; then
+        echo "usage: portshuffle.sh <max_port_number> <min_port_number>"
+else
+        if [ $2 -lt $1 ]; then
+                echo "Parameter 1 must be smaller than parameter 2."
+        else
+                newport=$(portshuffle $1 $2)            # Generate a new random port.
+                printf "Final: %s\n" $newport
+                #portchange $newport            # Change the ssh port to newport in the ssh configuration file.
+                #/etc/init.d/ssh restart                # Restart the daemon identified by a parameter.
+        fi
+fi
